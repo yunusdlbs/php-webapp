ebc476c9a9de2ef0f16466e9c5c02f71fcc11035
Yunus Delibaş <yunusdelibas@outlook.com>
2024-03-28 17:23:16 -0700

    php-webapp first commit.

--- webproje/yonetim/functions.php
+++ webproje/yonetim/functions.php
@@ -3,7 +3,7 @@
 
 function baglan(){
     //$dbPass = getenv('PASSWD');
-    $baglanti = mysqli_connect("db","admin","1234","devops_cloud");
+    $baglanti = mysqli_connect("db","admin","*****","devops_cloud");
     if($baglanti){
         //echo "Bağlandık ";
     }else{
